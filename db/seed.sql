16ab12cb74cbc82880013284b1aaadc11238f755
Fadil Abdella <fabdella03@gmail.com>
2019-03-05 08:56:21 -0500

    push

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devour) VALUES ('BLT', 1);
+INSERT INTO burgers (burger_name, devour) VALUES ('Turkey Cranberyy', 0);
+INSERT INTO burgers (burger_name, devour) VALUES ('Classic', 0);
