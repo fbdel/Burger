7a9d85527689663906ea10737db5fd20f813f2dc
Fadil Abdella <fabdella03@gmail.com>
2019-03-11 19:11:16 -0400

    11

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
     connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "yourRootPassword",
+    host: "pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    user: "	drvd3n2jp80ne73m",
+    password: "	gvv2w9701li383tq",
     database: "burgers_db"
 });
 };
